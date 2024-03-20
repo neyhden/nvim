a191ca9a118e30a8ff77b2b8d2747289acff0509
EliasHernandezAbreu <alu0101487137@ull.edu.es>
2024-03-20 16:27:28 +0000

    shortcuts

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,8 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-o>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<C-f>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fd", ":Telescope buffers<CR>", {})
 		end,
 	},
 	{
