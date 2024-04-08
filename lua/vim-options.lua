dbbd69ae0031b72ba41949897945faf60d0d7a48
EliasHernandezAbreu <alu0101487137@ull.edu.es>
2024-04-08 09:00:05 +0100

    tweaks

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,7 +7,17 @@ vim.cmd("set number")
 vim.cmd("set showtabline=2")
 vim.cmd("set cindent")
 vim.cmd("set cinoptions=2")
+-- ctrl keymaps
 vim.keymap.set("n", "<C-z>", "u", {})
+vim.keymap.set("n", "<C-c>", "\"+yy", {})
+vim.keymap.set("n", "<C-v>", "\"+p", {})
+vim.keymap.set("v", "<C-c>", "\"+y", {})
+
+-- Change movements (jk) to their "go" version
+vim.keymap.set("n", "j", "gj", {})
+vim.keymap.set("n", "k", "gk", {})
+vim.keymap.set({"n", "v"}, "gb", "[{", {})
+vim.keymap.set({"n", "v"}, "ge", "]}", {})
 
 vim.g.mapleader = " "
 
